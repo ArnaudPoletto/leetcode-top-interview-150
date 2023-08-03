73bd456ebdf3033ff2dbf1b9bfcd5175667359f7
Arnaud Poletto <arnaud.poletto@epfl.ch>
2023-08-03 22:05:28 +0200

    submit jump-game

---
+++ 01-array-string/09-jump-game/jump-game.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        idx = len(nums) - 1
+        for i in range(len(nums)-1, -1, -1):
+            if nums[i]+i >= idx:
+                idx = i
+        
+        return idx == 0
