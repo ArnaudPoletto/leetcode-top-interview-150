154eabf58519a0fda5c86535d08a43052f662211
Arnaud Poletto <arnaud.poletto@epfl.ch>
2023-08-03 21:13:16 +0200

    submit best-time-to-buy-and-sell-stock-ii

---
+++ 01-array-string/08-best-time-to-buy-and-sell-stock-ii/08-best-time-to-buy-and-sell-stock-ii.py.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # SOLUTION 1: using range
+        return sum(max(prices[i+1] - prices[i], 0) for i in range(0, len(prices)-1))
+
+        # SOLTUION 2: using pairwise
+        return sum(max(sell - buy, 0) for buy, sell in pairwise(prices))
